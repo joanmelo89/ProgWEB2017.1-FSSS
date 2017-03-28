ce00840b8ab59760001326c5345aed8ed1fabc38
joanmelo89 <joanmelo89@gmail.com>
2017-03-28 20:33:23 -0300

    MYSQL
    Inserção de dados no MYSQL

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,10 +1,23 @@
 <?php
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
-//pega os dados
-?>
 
-<?php include("cabecalho.php"); ?>
+$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+
+if(mysqli_query($conexao, $query)){?>
 <p class="alert-success">Produto <?php echo $nome ?>, <?php echo $preco ?> 
 adicionado com sucesso!</p>
+<?php } else{ ?>
+<p class="alert-danger">Produto não foi adicionado!</p>
+<?php	
+}
+mysqli_close($conexao);
+?>
+
+
+
+
+<?php include("cabecalho.php"); ?>
+
 <?php include("rodape.php"); ?>	
