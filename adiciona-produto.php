f21770908ac4a1c8c434f2ac6e44023f439ca2b6
joanmelo89 <joanmelo89@gmail.com>
2017-04-03 21:42:20 -0300

    Atualização
    Formulario e adiciona produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,23 +1,25 @@
+<?php include("cabecalho.php");?>
 <?php
+
+function insereProduto($conexao, $nome, $preco){
+	$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+	return mysqli_query($conexao, $query);
+	
+}
+
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
-
 $conexao = mysqli_connect('localhost', 'root', '', 'loja');
-$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
 
-if(mysqli_query($conexao, $query)){?>
-<p class="alert-success">Produto <?php echo $nome ?>, <?php echo $preco ?> 
-adicionado com sucesso!</p>
-<?php } else{ ?>
-<p class="alert-danger">Produto não foi adicionado!</p>
+if(insereProduto($conexao, $nome, $preco)){?>
+	<p class="text-success">Produto <?php echo $nome;?> , <?php echo $preco;?> adicionado com sucesso!</p>
+	
+<?php } else{ 
+$msg = mysqli_error($conexao);
+?>
+<p class="text-danger">O produto <?php $nome ?> não foi adicionado: <?php echo $msg;?></p>
 <?php	
 }
-mysqli_close($conexao);
+
 ?>
-
-
-
-
-<?php include("cabecalho.php"); ?>
-
-<?php include("rodape.php"); ?>	
+<?php include("rodape.php");?>
