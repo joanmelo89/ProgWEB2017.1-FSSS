a9fbf13306916bf79f7df5be76fc24e56b24be4a
joanmelo89 <joanmelo89@gmail.com>
2017-04-17 21:43:50 -0300

    Banco atualizado
    Adicionar campo descrição ao BD

--- loja.sql
+++ loja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Mar-2017 às 01:32
+-- Generation Time: 18-Abr-2017 às 02:43
 -- Versão do servidor: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -29,18 +29,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `produtos` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
-  `preco` decimal(10,2) DEFAULT NULL
+  `preco` decimal(10,2) DEFAULT NULL,
+  `descricao` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id`, `nome`, `preco`) VALUES
-(1, 'Carro', '10000.00'),
-(2, 'moto', '5000.00'),
-(3, 'bola', '10.00'),
-(4, 'arroz', '5.00');
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `descricao`) VALUES
+(7, 'carro', '200000.00', 'Aqui é a descrição do produto'),
+(8, 'Lapis do futuro', '999.00', 'Com essa lÃ¡pis vocÃª pode viajar no tempo!'),
+(9, 'Disco voador', '100.00', 'Disco voador que vem de marte.'),
+(10, 'ChapÃ©u', '15.00', 'ChapÃ©u com cÃ¢mera filmadora com qualidade 4k.'),
+(11, 'Bola', '40.00', 'Bola pula pula.'),
+(12, 'mouse', '50.00', 'Lorem Ipsum Ã© simplesmente uma simulaÃ§Ã£o de texto da indÃºstria tipogrÃ¡fica e de impressos, e vem sendo utilizado desde o sÃ©culo XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu nÃ£o sÃ³ a cinco sÃ©culos, como tambÃ©m ao salto para a editoraÃ§Ã£o eletrÃ´nica, permanecendo essencialmente inalterado. Se popularizou na dÃ©cada de 60, quando a Letraset lanÃ§ou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoraÃ§Ã£o eletrÃ´nica como Aldus PageMaker.');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +63,7 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
