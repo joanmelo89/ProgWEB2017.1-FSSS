6932860479d0f31cb93eca6b9e44a4cb6c2e0256
joanmelo89 <joanmelo89@gmail.com>
2017-05-22 21:29:33 -0300

    Atualização (login)
    Ação login

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 //Inclusão de arquivos PHP em outro arquivo PHP
 include("cabecalho.php"); 
 ?>
+<?php if(isset($_GET["login"]) && $_GET["login"] == true) {?>
+	<p class="alert-success">Logado com sucesso!</p>
+	<?php }?>
+<?php if(isset($_GET["login"]) && $_GET["login"] == 0) {?>
+	<p class="alert-danger">Usuário e senha inválidos!</p>
+	<?php }?>
 		<h1>Bem-vindo</h1>
+		<?php if(isset($_COOKIE["usuario_logado"])) {?>
+				<p class="alert-success">Você está logado como <?php echo $_COOKIE["usuario_logado"]?>.</p>
+				<?php }else{?>
 		<h2>Login</h2>
 		
 		<form action="login.php" method="post">
@@ -20,5 +29,5 @@ include("cabecalho.php");
 			</tr>
 		</table>
 		</form>
-
+		<?php }?>
 <?php include("rodape.php"); ?>	
--- login.php
+++ login.php
@@ -4,5 +4,12 @@ include("banco-usuario.php");
 
 
 $usuario = buscaUsuario($conexao, $_POST['usuario'], $_POST['senha']);
+setcookie("usuario_logado", $usuario["email"], time() + 60);
+if($usuario == null){
+	header("Location: index.php?login=0");
+}else{
+	header("Location: index.php?login=1");
+}
+die();
 
 ?>
